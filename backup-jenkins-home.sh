d38d47b6f7481c35f9dbb9f70083f0dd8f6d2066
root <root@ip-172-31-23-186.ap-south-1.compute.internal>
2021-08-12 10:05:30 +0000

    added backup-jenkins-home.sh

--- backup-jenkins-home.sh
+++ backup-jenkins-home.sh
@@ -3,4 +3,4 @@
 DATE=$(date +%S-%M-%H-%d-%m-%y)
 BACKUP_DIR=/var/lib/jenkins/backup
 
-tar -czvf $BACKUP_DIR/shell-scripts-$DATE.tar.gz /var/lib/jenkins/my-shell-scripts
+tar -czvf $BACKUP_DIR/shell-scripts-$DATE.tar.gz /var/lib/jenkins/workspace

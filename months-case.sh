38f7f92095e8cd92a91c1875ce15b7d4b066d6f4
root <root@ip-172-31-23-186.ap-south-1.compute.internal>
2021-08-12 12:40:42 +0000

    adding months-case.sh

--- months-case.sh
+++ months-case.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
-echo " enter a number [0-11]"
-read n 
+
 case $n in
-                0)
-		echo "jan" :;
-		1)
+	 0)
+
+	     	echo "jan" :;
+         1) 
+
 	        echo "feb" :;
-		2)
+         2)
+
 		echo "march" :;
-		3)
+
+	 3)
 		echo "april" :;
-		4)
+
+	 4)
 		echo "may" :;
-		5)
-		echo "june" :;
-		6)
+
+         5)
+
+	        echo "june" :;
+         6)  
+
 		echo "july" :;
-		7)
+
+         7)
 		echo "august" :;
-		8)
+
+	 8)
 		echo "september" :;
-		9)
-		echo "october" :;
-		10)
+
+         9)
+	 
+ 	         echo "october" :;
+
+         10)
 	       echo " november" :;
-	       11)
+
+	 11)
                echo " december" :;	
 
 esac
